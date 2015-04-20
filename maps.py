d0e9901e2b50347b970e4754da43e50ec14f7ac7
Jason Hu <hu@hu.local>
2015-04-20 12:36:17 -0400

    Made rudimentary function to split roadtrip into days

--- maps.py
+++ maps.py
@@ -1,24 +1,90 @@
 import urllib
+import json
+
 
 def main():
-	url = "http://maps.googleapis.com/maps/api/directions/json?"
-	origin = "origin=Toronto"
-	destination = "destination=Montreal"
-	assembledURL = url + origin + "&" + destination
-	print urllib.urlopen(assembledURL).read()
+	fullJourney = makeRequest("Providence,RI", "Miami,FL")
+	dayJourneys = splitRouteIntoDays(fullJourney)
+	for day in dayJourneys:
+		print day["start_location"], "->", day["end_location"]
+	meals = [findMealLocation]
+	for time, coordinate in meals:
+		print "We'll be eating at", coordinate, "at", time
 
+
+""" Makes the API request, parses it, and returns a pathObject
+"""
+def makeRequest(startLocation, endLocation):
+
+	# Assembling the API request. Maybe we should do some
+	# input checking?
+	url = "http://maps.googleapis.com/maps/api/directions/json?"
+	origin = "origin=" + startLocation
+	destination = "destination=" + endLocation
+	assembledURL = url + origin + "&" + destination
+	response = json.loads(urllib.urlopen(assembledURL).read())
+	if response["status"] != "OK":
+		print "Warning! Status is not okay" # should throw error?
+		return None
+
+	# See https://developers.google.com/maps/documentation/directions/#DirectionsResponses
+	# for details about the fields returned by the API request.
+	return parseLegHelper(response["routes"][0]["legs"][0])
+
+def parseLegHelper(leg):
+	# A leg as defined by Google is a poriton of the path, 
+	# if we use waypoints. Otherwise, it's the whole path
+	pathObjectFields = ["distance", "duration", "start_location", "end_location"]
+
+	# A step is the smallest fragement of the journey.
+	steps = [{key: step[key] for key in pathObjectFields} for step in leg["steps"]]
+
+	pathObject = {key: leg[key] for key in pathObjectFields}
+	pathObject["steps"] = steps
+
+	return pathObject
+
+# Returns the index of the step in pathObject["steps"]
+# where you'll be after traveling desiredMins
+def findTimeHelper(steps, desiredMins):
+
+	steps = pathObject["steps"]
+	minsTraveledToday = 0
+	for i, step in enumerate(steps):
+		minsTraveledToday += step["duration"]["value"]
+		if minsTraveledToday > desiredMins:
+			return i
+
+	return len(steps)
+
+""" Returns a list of new pathObjects
+"""
+def splitRouteIntoDays(pathObject, minHours=6, maxHours=12):
+
+	days = []
+
+	idealMinutes = ((minHours + maxHours) / 2) * 60
+
+	steps = pathObject["steps"]
+	while len(steps) > 0:
+		i = findTimeHelper(steps, idealMinutes)
+		day = {	"distance": {}, 
+				"duration": {}, 
+				"start_location": steps[0]["start_location"], 
+				"end_location": steps[i]["end_location"], 
+				"steps": steps[:i+1]}
+
+	return days
 
 """ Returns the list of (time, coordinates), if applicable
 
 pathObject - ???
 """
-def findMealLocation(pathObject, departureTime, breakfast=9, lunch=12, dinner=18):
+def findMealLocation(pathObject, departureTime=9, lunch=12, dinner=18):
 	pass
 
-""" Returns a list of new pathObjects
-"""
-def splitRouteIntoDays(pathObject, maxHours=12):
-	pass
 
+
+	pass
 if __name__ == '__main__':
 	main()
