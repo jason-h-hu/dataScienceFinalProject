c94ca88ee19968ad7974baa2778ed0b685341249
Jason Hu <hu@hu.local>
2015-04-20 20:59:43 -0400

    This kinda works. Barely.

--- maps.py
+++ maps.py
@@ -1,15 +1,27 @@
 import urllib
 import json
+import math
+
+def printAsTime(totalSeconds):
+	hours = int(math.floor(totalSeconds/(60*60)))
+	mins = totalSeconds/60 - hours*60
+	return str(hours) + ":" + str(mins)
 
 
 def main():
-	fullJourney = makeRequest("Providence,RI", "Miami,FL")
+	fullJourney = makeRequest("Providence,RI", "San Francisco,CA")
 	dayJourneys = splitRouteIntoDays(fullJourney)
+
 	for day in dayJourneys:
-		print day["start_location"], "->", day["end_location"]
-	meals = [findMealLocation]
-	for time, coordinate in meals:
-		print "We'll be eating at", coordinate, "at", time
+		# print sum([step["duration"]["value"] for step in day["steps"]])/(60*60)
+		elapsed = 9*60*60
+		for step in day["steps"]:
+			elapsed += step["duration"]["value"]
+			print printAsTime(elapsed), "\t->\t",step["distance"]["value"]
+		meals = findMealLocation(day)
+		for time, coordinate in meals:
+			print "We'll be eating at", coordinate["lat"], ",", coordinate["lng"], "at", printAsTime(time)
+		print "next day"
 
 
 """ Makes the API request, parses it, and returns a pathObject
@@ -26,7 +38,6 @@ def makeRequest(startLocation, endLocation):
 	if response["status"] != "OK":
 		print "Warning! Status is not okay" # should throw error?
 		return None
-
 	# See https://developers.google.com/maps/documentation/directions/#DirectionsResponses
 	# for details about the fields returned by the API request.
 	return parseLegHelper(response["routes"][0]["legs"][0])
@@ -46,15 +57,14 @@ def parseLegHelper(leg):
 	return pathObject
 
 # Returns the index of the step in pathObject["steps"]
-# where you'll be after traveling desiredMins
-def findTimeHelper(steps, desiredMins):
+# where you'll be after traveling desiredSeconds
+def findTimeHelper(steps, desiredSeconds):
 
-	steps = pathObject["steps"]
-	minsTraveledToday = 0
+	secsTraveledToday = 0
 	for i, step in enumerate(steps):
-		minsTraveledToday += step["duration"]["value"]
-		if minsTraveledToday > desiredMins:
-			return i
+		secsTraveledToday += step["duration"]["value"]
+		if secsTraveledToday > desiredSeconds:
+			return i+1
 
 	return len(steps)
 
@@ -64,18 +74,19 @@ def splitRouteIntoDays(pathObject, minHours=6, maxHours=12):
 
 	days = []
 
-	idealMinutes = ((minHours + maxHours) / 2) * 60
+	idealSeconds = ((minHours + maxHours) / 2)*60*60
 
 	steps = pathObject["steps"]
 	while len(steps) > 0:
-		i = findTimeHelper(steps, idealMinutes)
+		i = findTimeHelper(steps, idealSeconds)
 		currentSteps = steps[:i]
 		day = {	"distance": {"value": sum([step["distance"]["value"] for step in currentSteps])}, 
 				"duration": {"value": sum([step["duration"]["value"] for step in currentSteps])}, 
-				"start_location": steps[0]["start_location"], 
+				"start_location": currentSteps[0]["start_location"], 
 				"end_location": currentSteps[-1]["end_location"], 
 				"steps": currentSteps}
-		steps = steps[i+1:]
+		days.append(day)
+		steps = steps[i:]
 
 	return days
 
@@ -84,7 +95,18 @@ def splitRouteIntoDays(pathObject, minHours=6, maxHours=12):
 pathObject - ???
 """
 def findMealLocation(pathObject, departureTime=9, lunch=12, dinner=18):
-	pass
+
+	meals = []
+	steps = pathObject["steps"]
+	l = findTimeHelper(steps, (lunch-departureTime)*60*60)
+	lunchtime = departureTime*60*60 + sum([step["duration"]["value"] for step in steps[:l]])
+	lunchlocation = steps[l-1]["end_location"]
+
+	d = findTimeHelper(steps, (dinner-departureTime)*60*60)
+	dinnertime = departureTime*60*60 + sum([step["duration"]["value"] for step in steps[:d]])
+	dinnerlocation = steps[d-1]["end_location"]
+
+	return [(lunchtime, lunchlocation), (dinnertime, dinnerlocation)]
 
 
 
