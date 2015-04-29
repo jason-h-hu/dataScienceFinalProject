d7b1eb4bda7177443637e9807829b612667536eb
SarahCIT <sarahmsachs@gmail.com>
2015-04-29 16:23:47 -0400

    Deleted

--- reviews.py
+++
@@ -1,8 +0,0 @@
-"""from jatbar
-https://github.com/mrorii/findjatbar/blob/master/findjatbar/reviews.py
-"""
-#!/usr/bin/env python
-
-from collections import namedtuple
-
-Review = namedtuple('Review', 'restaurant_id, author, entry')
