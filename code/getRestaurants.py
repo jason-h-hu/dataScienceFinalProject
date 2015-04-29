47a17b17bd9945c474d0cf460c91419021ed756d
SarahCIT <sarahmsachs@gmail.com>
2015-04-29 16:11:04 -0400

    getRestaurants now has full functionality and can be called with a lat and lng and returns a list of resturants, each with a dictionary following our preditermined format

---
+++ code/getRestaurants.py
@@ -0,0 +1,47 @@
+import yelp
+import argparse
+import urllib
+import googlePlaces
+import urllib2
+
+# input: latitude (float) and longitude (float)
+# output: list of restaurants around that lat and lng
+# restaurant = {coord:{lat:<float>,lng:<float>}, img_url:<str>, name:<string>, phone_num:<str>,address:<list<str>>, rest_url:<str>, num_yelp_reviews:<int>, dist_from_ll:<float>, review_list:[{review_author:<str>,review_star:<float>,review_text:<str>},{},..], google_star:<float>, yelp_star:<float>,google_place_id:<str>,yelp_id:<str>}
+#if restaurant doesn't have google review, then it won't have review_list, google star, or goolge place id in its dictionary
+# google place {reviews,google_place_id, google_star}
+
+SARAH_HOUSE_LAT = 41.826998
+SARAH_HOUSE_LNG = -71.403599
+
+def get_restaurants(lat=SARAH_HOUSE_LAT,lng=SARAH_HOUSE_LNG):
+    ll = str(lat) + "," + str(lng)
+    restaurant_list = yelp.query_api(ll)
+    for restaurant in restaurant_list:
+        lat = None
+        lng = None
+        name = None
+        if 'coord' in restaurant:
+            lat = restaurant['coord']['latitude']
+            lng = restaurant['coord']['longitude']
+        if 'name' in restaurant:
+            name = restaurant['name']
+        if lat and lng and name:
+            google_dict = googlePlaces.queryGoogle(lat, lng, name)
+            if (google_dict!=None):
+                restaurant['google_place_id'] = google_dict['google_place_id']
+                restaurant['google_rating'] = google_dict['googe_rating']
+                restaurant['google_review_list'] = google_dict['reviews']
+    return restaurant_list
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-lat', '--lat', dest='lat', default=SARAH_HOUSE_LAT, type=float, help='Search location latitude (default: %(default)s)')
+    parser.add_argument('-lng', '--lng', dest='lng', default=SARAH_HOUSE_LNG, type=float, help='Search location longitude (default: %(default)s)')
+    
+    input_values = parser.parse_args()
+    get_restaurants(input_values.lat,input_values.lng)
+
+if __name__ == '__main__':
+    main()
---
+++ code/googlePlaces.py
@@ -0,0 +1,110 @@
+# usage example: python googlePlay.py --lat 37.878998 --lng -122.182021 --name "La Piazza"
+
+
+#*Functions to grab the necessary data from Google.
+#BEGIN PROGRAM.
+import urllib, json
+import argparse
+
+
+
+#Grabbing and parsing the JSON data
+def get_place_id(lat,lng,name):
+  #making the url
+  AUTH_KEY = "AIzaSyAOjjN8YD3ZudTfA1miPPY3Wm1S7Zla8Dk"
+  LOCATION = str(lat) + "," + str(lng)
+  RADIUS = 10 #radius
+  TYPES = "restaurant"
+  NAME = name
+  MyUrl = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+           '?location=%s'
+           '&radius=%s'
+           '&types=%s'
+           '&name=%s'
+           '&sensor=false&key=%s') % (LOCATION, RADIUS, TYPES, NAME, AUTH_KEY)
+  #grabbing the JSON result
+  response = urllib.urlopen(MyUrl)
+  jsonRaw = response.read()
+  jsonData = json.loads(jsonRaw)
+  if ('results' in jsonData):
+    if len(jsonData['results'])==0:
+      return None
+    elif ('rating' in jsonData['results'][0]):
+      return (jsonData['results'][0]['place_id'], jsonData['results'][0]['rating'])
+    else:
+      return None
+  else:
+    return None
+
+#This is a helper to grab the Json data that I want in a list
+def IterJson(place):
+  x = [place['name'], place['reference'], place['geometry']['location']['lat'], 
+         place['geometry']['location']['lng'], place['vicinity']]
+  return x
+#END PROGRAM.
+
+def get_reviews(place_id):
+  PLACE_ID = str(place_id)
+  AUTH_KEY = "AIzaSyAOjjN8YD3ZudTfA1miPPY3Wm1S7Zla8Dk"
+  MyUrl = ('https://maps.googleapis.com/maps/api/place/details/json'
+           '?placeid=%s'
+           '&sensor=false&key=%s') % (PLACE_ID, AUTH_KEY)
+  #grabbing the JSON result
+  response = urllib.urlopen(MyUrl)
+  jsonRaw = response.read()
+  jsonData = json.loads(jsonRaw)
+  reviewData = jsonData['result']['reviews']
+  review_list = []
+  for review in reviewData:
+    review_dict = {}
+    review_dict['review_star']=review['rating']
+    review_dict['review_author']=review['author_name']
+    review_dict['review_text'] = review['text']
+    review_list.append(review_dict)
+  return review_list
+
+
+def queryGoogle(lat, lng, name):
+  if (get_place_id(lat, lng, name)!=None):
+    (place_id,rating) = get_place_id(lat, lng, name)
+    review_list=get_reviews(place_id)
+    googleDict = {}
+    googleDict['google_place_id']=place_id
+    googleDict['googe_rating']=rating
+    googleDict['reviews'] = review_list
+    return googleDict
+  else:
+    return None
+  
+def main():
+   
+    east_side_lat  = 41.829554
+    east_side_lng  = -71.400978
+    east_side_name = "East Side Pockets"
+
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-lat', '--lat', dest='lat', default=east_side_lat, type=float, help='Search Latitude (default: %(default)s)')
+    parser.add_argument('-lng', '--lng', dest='lng', default=east_side_lng, type=float, help='Search Longitude (default: %(default)s)')
+    parser.add_argument('-name', '--name', dest='name', default=east_side_name, type=str, help='Search Name (default: %(default)s)')
+
+    input_values = parser.parse_args()
+
+    try:
+        (place_id,rating) = get_place_id(input_values.lat,input_values.lng,input_values.name)
+        review_list=get_reviews(place_id)
+        googleDict = {}
+        googleDict['google_place_id']=place_id
+        googleDict['googe_rating']=rating
+        googleDict['reviews'] = review_list
+        return googleDict
+
+        # query_api(LL)
+    except urllib2.HTTPError as error:
+        sys.exit('Encountered HTTP error {0}. Abort program.'.format(error.code))
+
+
+
+if __name__ == '__main__':
+    main()
