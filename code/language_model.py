6f4b7ee6795631107fb38d135050ea886ca1cd55
Jacqueline Stoermer <jstoerme@cs.brown.edu>
2015-04-29 16:18:42 -0400

    Adding language modeling and unique words files

---
+++ code/language_model.py
@@ -0,0 +1,105 @@
+"""
+unique words python
+input: corpus file of restaurant reviews
+output: dictionary of words and their probabilities
+
+This smooths for unknown words by taking all words that appear less than 4 times 
+and changing them to "*UNK*", then finding a probability for "*UNK*"
+  
+"""
+
+import sys
+import math
+from collections import defaultdict
+import shelve
+
+
+prob_dict = defaultdict(lambda:0.0)
+word_dict = defaultdict(lambda:0.0)
+word_count = 0.0
+
+
+"""
+Input: a file name to count the words of
+Output: None.
+Populates global word count with words as keys and number of occurences as value
+"""
+
+def word_count(input_file):
+        global word_dict
+        global word_count
+        counter = 0
+        #Goes through lines of file, adding them to dictionary
+        for line in input_file:
+            #Goes through lines and adds a start and stop character
+            line_arr = line.split()
+            line_arr.insert(0, ">>")
+            line_arr.append("<<")
+            
+            for word in line_arr: 
+            # Counts words and updates dictionary
+                counter += 1
+                word_dict[word] += 1
+                
+        word_count = counter
+        # Close file
+        input_file.close()
+
+
+
+"""
+set_prob
+
+Input: Alpha, a parameter for smoothing unigram probabilities
+Output: None
+Populates global prop_dict with the smoothed probabilities for all single words
+"""
+def set_prob():
+    global word_dict
+    global prob_dict
+    
+    divisor =word_count
+
+    for word in word_dict:
+        #sets the probability of each word to the frequency of word 
+        # divided by the total word count
+        theta_w = word_dict[word]/divisor
+        prob_dict[word] = theta_w
+"""
+clean_prob
+
+Goes through and makes all words that appear less than 3 times *UNK*
+
+"""
+
+def clean_words():
+    global word_dict
+
+    for word in word_dict.keys():
+        if word_dict[word] <= 3:
+            word_dict["*UNK*"] += word_dict[word]
+            del word_dict[word]
+
+
+def main(argv):
+    # Checks 
+    if len(argv) != 2:
+        print "Please enter input file."
+    else:
+        training = open(argv[1])
+
+        #necessary set up to populat global variables
+        #if data is changed, these functions must be re-run
+        word_count(training)
+        clean_words()
+        set_prob()
+        probs_to_save = shelve.open("prob_dict_shelf.txt", writeback=True)
+        
+        for p in prob_dict:
+            probs_to_save[p] = prob_dict[p]
+
+
+        probs_to_save.close()
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ code/unique_word_builder.py
@@ -0,0 +1,54 @@
+"""
+Unique Word Builder
+Input: dictionary of restaurants for one location
+
+restaurant = {coord:{lat:<float>,lng:<float>}, img_url:<str>, name:<string>, phone_num:<str>,
+        address:<list<str>>, rest_url:<str>, num_yelp_reviews:<int>, dist_from_ll:<float>, yelp_review:<str>, 
+        google_review_list:[{review_author:<str>,review_star:<float>,review_text:<str>},{},..], 
+        google_star:<float>, yelp_star:<float>,google_place_id:<str>,yelp_id:<str>}
+
+
+output: dictionary of restaurants for one location with an added "unique_words" field
+"""
+
+from collections import defaultdict
+import data_cleaning
+import shelve
+import operator
+
+
+def build_text_field(rest_dict):
+	text_string = restaurant["yelp_review"]
+	for review in restaurant["google_review_list"]:
+		text_string = text_string+" "+restaurant["google_review_list"]["review_text"]
+
+	text_string = data_cleaning.clean(text_string)
+	return text_string.split()
+
+
+
+def determine_unique_words(rest_text, prob_dict):
+	interesting_words_dict = defaultdict()
+	for word in rest_text:
+		if word in prob_dict:
+			interesting_words_dict[word] += (1/prob_dict[word])
+
+	sorted_words = sorted(interesting_words_dict.items(), key=operator.itemgetter(1))
+	unique_words =  sorted_words[:10]
+	return unique_words
+
+
+def build_words_entry(prob_dict, location_list):
+
+	for restaurant in location_list:
+		prob_dict = shelve.open.(prob_dict_shelf)
+		rest_text = build_text_field(prob_dict, restaurant)
+		unique_words = determine_unique_words(rest_text, prob_dict)
+		restaurant["unique_words"] = unique_words
+
+		prob_dict.close()
+
+	return location_list
+
+
+
