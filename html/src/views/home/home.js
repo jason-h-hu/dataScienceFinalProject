48a971f3af6db78b1e4ebbfd24490bb65f8a34f2
Dylan Gattey <dylan.gattey@gmail.com>
2015-05-04 22:57:48 -0400

    Status on loading route and restaurants (see button)

--- html/src/views/home/home.js
+++ html/src/views/home/home.js
@@ -3,12 +3,15 @@ angular.module('app.views.home', [])
 	// Set title of page
 	$rootScope.pageTitle = "Home";
 
+	$scope.buttonText = 'Get Route!';
+
 	// Testing purposes, maybe date is a good one to keep in
 	$scope.start = "Providence, RI";
 	$scope.end = "San Francisco, CA";
 	$scope.date = new Date();	
 
 	function clear() {
+		$scope.segment = undefined;
 		$scope.restaurantOps = undefined;
 		$scope.mealLocations = undefined;
 	}
@@ -23,6 +26,11 @@ angular.module('app.views.home', [])
 		}).success(function (data, status) {
 			if ($scope.restaurantOps === undefined) $scope.restaurantOps = {};
 			$scope.restaurantOps[date] = data; // List of restaurant objects
+			$scope.segment = $scope.segment + 1;
+			if ($scope.segment > $scope.mealLocations.length) {
+				$scope.buttonText = 'Get Route!';
+				$scope.segment = undefined;
+			}
 		}).error(function (data, status) {
 			console.log('Error getting restaurant for', date, rest, ':', data, status);
 		});
@@ -37,6 +45,7 @@ angular.module('app.views.home', [])
 
 	// Saves journey from start to end to $scope.mealLocations
 	$scope.getJourney = function() {
+		$scope.buttonText = 'Routing...';
 		clear();
 		$http({
 			url: 'http://localhost:5000/journey',
@@ -45,6 +54,8 @@ angular.module('app.views.home', [])
 			headers: {'Content-Type': 'application/json'}
 		}).success(function (data, status) {
 			$scope.mealLocations = data;
+			$scope.buttonText = 'Loading restaurants';
+			$scope.segment = 1;
 			$scope.getRestaurants();
 		}).error(function (data, status) {
 			console.log('Error getting journey:', data, status);
