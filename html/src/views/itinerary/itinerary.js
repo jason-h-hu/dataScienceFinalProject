acc4307c48ffc3f0787c66e46dcd6a78cb983e23
Dylan Gattey <dylan.gattey@gmail.com>
2015-05-09 01:38:19 -0400

    Fixed colors and hovers

--- html/src/views/itinerary/itinerary.js
+++ html/src/views/itinerary/itinerary.js
@@ -1,4 +1,10 @@
 angular.module('app.views.itinerary', [])
-  .controller('ItineraryCtrl', function($scope){
+  .controller('ItineraryCtrl', function($scope, $rootScope){
+  	
+  	$scope.select = function(index) {
+  		var location = $rootScope.locations[index];
+  		if (!$rootScope.itinerary[location[0]]) return;
+  		$rootScope.mealStats.curr = index;
+  	}
 
   });
