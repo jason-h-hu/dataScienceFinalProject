7f377101ad645a1cee7da673d87dffce55545e9a
SarahCIT <sarahmsachs@gmail.com>
2015-05-10 22:37:55 -0400

    added metrics for classifier

--- code/build_classifier.py
+++ code/build_classifier.py
@@ -1,3 +1,4 @@
+import util
 import csv
 import time
 import google_places
@@ -7,6 +8,14 @@ from sklearn import linear_model
 import yelp
 import ranking
 import json
+from sklearn import datasets
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+from sklearn import cross_validation
+from sklearn import metrics
+import random
+from sklearn import linear_model
+from sklearn import preprocessing
 
 
 
@@ -21,14 +30,30 @@ def regression():
         jsonData = json.load(f)
     X_train = jsonData['X_train']
     Y_train = jsonData['Y_train']
+    Y_train = np.array(Y_train)
+    X_train = np.array(X_train)
+    X_train = preprocessing.scale(X_train)
     regr = linear_model.LinearRegression()
     regr.fit(X_train, Y_train)
 
     # The coefficients
     print('Coefficients: \n', regr.coef_)
-    # print(" X_test = [[5.0,0,1000],[5.0,1000,1000],[4.5,0,450], [4.5,-50,450],[4.9,0,455],[4.6,0,2],[4.6,2.1,2],[4.6,2.1,2],[1,0,5000],[1,25,5000],[1.1,0,5],[2,0,300]]")
-    X_test = [[5.0,0,1000],[5.0,1000,1000],[4.5,0,450], [4.5,-50,450],[4.9,0,455],[4.6,0,2],[4.6,2.1,2],[4.6,2.1,2],[1,0,5000],[1,25,5000],[1.1,0,5],[2,0,300]]
-    # print regr.predict(X_test)
+
+    # The mean square error
+    print("Residual sum of squares: %.2f"
+          % np.mean((regr.predict(X_train) - Y_train) ** 2))
+    print ("score of regression is"+str(regr.score(X_train, Y_train)))
+    meanDiffs = []
+    
+    print X_train.shape
+    for i in range(10):
+        randomNum = random.randrange(1,10)
+        X_CV_train, X_CV_test, y_CV_train, y_CV_test = cross_validation.train_test_split(X_train, Y_train, test_size=0.1, random_state=0)
+        CVregr = linear_model.LinearRegression().fit(X_CV_train, y_CV_train)
+        meanDiffs.append(np.mean((CVregr.predict(X_CV_test) - y_CV_test) ** 2))
+    cvSquaredError = np.mean(meanDiffs)
+    print ("cv mean squared error is" + str(cvSquaredError))
+    
     return regr
 
 
